dace528d5c5de0b312d53090d0835c82e1ee2fb3
venmos <venmos@fuck.gfw.es>
2017-04-27 03:01:51 +0800

    Modify the shortcut key

--- packages.el
+++ packages.el
@@ -41,41 +41,53 @@
     :commands (helm-w3m-bookmarks)
     :init
     (progn
-      (evil-leader/set-key
+      (spacemacs/set-leader-keys
         "awb" 'helm-w3m-bookmarks))))
 
-(defun v/w3m-open-site (site)
-  "Opens site in new w3m session with 'http://' appended"
+(defun v/w3m-open-url (url)
+  "Opens url in new w3m session with 'http://' appended"
   (interactive
    (list (read-string "Enter website address (default: google.co.jp):" nil nil "google.co.jp" nil )))
   (w3m-goto-url
-   (concat "http://" site)))
+   (concat "http://" url)))
 
-(defun v/w3m-open-site-new-session (site)
-  "Opens site in new w3m session with 'http://' appended"
+(defun v/w3m-open-url-new-session (url)
+  "Opens url in new w3m session with 'http://' appended"
   (interactive
-   (list (read-string "Enter website address (default: google.co.jp):" nil nil "google.co.jp" nil )))
+   (list (read-string "Enter website address (default: google.com):" nil nil "google.com" nil )))
   (w3m-goto-url-new-session
-   (concat "http://" site)))
+   (concat "http://" url)))
 
 (defun w3m/init-w3m()
   "Initializes w3m and adds keybindings for its exposed functionalities."
   (use-package w3m
+    :defer t
     :init
+    (spacemacs/set-leader-keys
+      "awo" 'v/w3m-open-url
+      "awf" 'w3m-find-file
+      "aws" 'w3m-search
+      )
     (progn
-      (spacemacs/set-leader-keys
-        "awo" 'v/w3m-open-site
-        "awg" 'w3m-goto-url
-        "awG" 'w3m-goto-url-new-session
-        "aws" 'w3m-search
-        "awS" 'w3m-search-new-session
         (evilified-state-evilify-map w3m-mode-map
           :mode w3m-mode
           :eval-after-load w3m
           :bindings
-          "wh" 'w3m-next-buffer
-          "wl" 'w3m-tab-next-buffer)
+          "wf" 'w3m-find-file
+          "wo" 'v/w3m-open-url
+          "wO" 'v/w3m-open-url-new-session
+          "wt" 'w3m-view-this-url-new-session
+          "wT" 'w3m-create-empty-session
+          "ws" 'w3m-search
+          "wS" 'w3m-search-new-session
+          "wl" 'w3m-next-buffer
+          "wh" 'w3m-previous-buffer
           "wx" 'w3m-delete-buffer
+          "we" 'w3m-bookmark-edit
+          "wa" 'w3m-bookmark-add-current-url
+          "wm" 'w3m-view-url-with-external-browser
+          "wb" 'helm-w3m-bookmarks
+          "wB" 'w3m-bookmark-view
         ))))
 
 (with-eval-after-load 'w3m
